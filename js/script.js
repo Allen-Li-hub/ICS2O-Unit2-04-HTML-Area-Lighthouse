f3b755253c70fe2b48c761b7d822660f7c0b7130
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-25 14:19:27 -0400

    change

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit2-04-HTML-Area-Lighthouse/sw.js", {
+    scope: "/ICS2O-Unit2-04-HTML-Area-Lighthouse/",
   })
 }
 
